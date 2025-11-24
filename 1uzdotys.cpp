d091b961f67aa195c27781c4fbc081405c2d6fd0
DaniilBolshoj <158136050+DaniilBolshoj@users.noreply.github.com>
2025-11-24 10:41:09 +0000

    Learning C++ Basics

---
+++ 1uzdotys.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Irašyk juodos skylės masę:";
+    cin >> n;
+    cout << "Juodos skylės spindulys: " << 5*n << " šviesmečių" << endl;
+    return 0;
+
+}
