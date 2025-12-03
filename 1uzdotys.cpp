704e3bbc09867a197a2e355083e2cc1dc17f6838
DaniilBolshoj <158136050+DaniilBolshoj@users.noreply.github.com>
2025-12-03 10:34:00 +0000

    Learning C++

--- 1uzdotys.cpp
+++ 1uzdotys.cpp
@@ -87,10 +87,109 @@ int main(){
 }
 */
 
+/*
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
+    double a, b, suma, skirtumas, sandauga, dalmuo, laipsnis, saknis;
+    cout << "Įvesk du numerius: ";
+    cin >> a >> b;
     
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " ^ " << b << " = " << pow(a, b) << endl;
+    cout << "√" << a << " = " << sqrt(a) << endl;
+    cout << "√" << b << " = " << sqrt(b) << endl;
+    return 0;
 }
+*/
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double a, b, c;
+    cout << "Įvesk tris numerius: ";
+    cin >> a >> b >> c;
+
+    cout << "Pirmas reiskinys: " << a + b * c << endl;
+
+    return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cout << "Įvesk tris numerius: ";
+    cin >> a >> b >> c;
+
+    cout << "Antras reiskinys: " << (a+b)%c << endl;
+
+    return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d;
+    cout << "Įvesk keturis numerius: ";
+    cin >> a >> b >> c >> d;
+
+    cout << "Trecias reiskinys: " << a + b * c / d << endl;
+
+    return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, b, c, d, e, f;
+    cout << "Įvesk sesis numerius: ";
+    cin >> a >> b >> c >> d >> e >> f;
+    
+
+    cout << "Ketvirtas2 reiskinys: " << a + b / c * d - e % f << endl;
+
+    return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a, pirmas_skaicius, antras_skaicius;
+    cout << "Įvesk numeri: ";
+    cin >> a;
+
+    pirmas_skaicius = a / 10;
+    antras_skaicius = a % 10;
+    
+
+    cout << a << " -> " << pirmas_skaicius << " + " << antras_skaicius << " = " <<pirmas_skaicius + antras_skaicius << endl;
+
+    return 0;
+}
+*/
+
