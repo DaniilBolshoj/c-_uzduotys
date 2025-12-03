308725c0568a719637fc0ca04832f64e346697a3
DaniilBolshoj <158136050+DaniilBolshoj@users.noreply.github.com>
2025-12-03 09:27:42 +0000

    Just learning c++

--- 1uzdotys.cpp
+++ 1uzdotys.cpp
@@ -1,12 +1,96 @@
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double squireit(double);
+double cubeit(double);
+
+int main(){
+    double x;
+    cout << "Įvesk numerį: ";
+    
+    cin >> x;
+    cout << squireit(x) << endl;
+    cout << cubeit(x) << endl;
+    
+    return 0;
+}
+
+double squireit(double number){
+    
+    double x = number;
+    
+    return x = x * x;
+}
+
+double cubeit(double number){
+    
+    double x = number;
+    
+    return x = x * x * x;
+}
+*/
+
+/*
 #include <bits/stdc++.h>
 
 using namespace std;
 
 int main(){
-    int n;
-    cout << "Irašyk juodos skylės masę:";
-    cin >> n;
-    cout << "Juodos skylės spindulys: " << 5*n << " šviesmečių" << endl;
+    double x, y, z;
+    cout << "Įvesk numerius: ";
+    
+    cin >> x >> y >> z;
+    cout << x + y + z << endl;
+    cout << x * y * z << endl;
+    
     return 0;
+}
+*/
 
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double x, y, suma, skirtumas, sandauga, dalmuo;
+    cout << "Įvesk du numerius: ";
+    cin >> x >> y;
+
+    suma = x + y;
+    skirtumas = x - y;
+    sandauga = suma * skirtumas;
+    dalmuo = suma / skirtumas;
+    cout << "Suma: " << suma << endl;
+    cout << "Skirtumas: " << skirtumas << endl;
+    cout << "Sandauga: " << sandauga << endl;
+    cout << "Dalmuo: " << dalmuo << endl;
+    return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    double a, b, c, d, vidurkis;
+    cout << "Įvesk keturis numerius: ";
+    cin >> a >> b >> c >> d;
+    vidurkis = (a + b + c + d) / 4;
+    cout << "Vidurkis: " << vidurkis << endl;
+    return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
 }
